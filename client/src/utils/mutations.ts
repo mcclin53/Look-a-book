0265d65aad5353ce76eb4efef89f7de8d55edda5
mcclin53 <dan.mcclintic@gmail.com>
2025-05-08 00:36:02 -0400

    updated mutations

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -1,9 +1,43 @@
 import { gql } from '@apollo/client';
 
-export const LOGIN_USER
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user
+                _id
+                username
+                savedBooks
+            }
+        }
+    `;
 
-export const ADD_USER
+export const ADD_USER = gql`
+mutation addUser($input: UserInput!) 
+    addUser(input: $input) {
+        token
+        user {
+            _id
+            username
+            savedBooks
+        }
+    }
+`;
 
-export const SAVE_BOOK
+export const SAVE_BOOK = gql`
+    mutation saveBook($userID: ID!, $savedBooks: String!) {
+        saveBook(userID: $userID, savedBooks: $savedBooks) {
+            _id
+            savedBooks
+        }
+    }
+`;
 
-export const REMOVE_BOOK
+export const REMOVE_BOOK = gql`
+    mutation removeBook($savedBooks: String!) {
+        removeBook(savedBooks: $savedBooks) {
+            _id
+            savedBooks
+        }
+    }
+`;
