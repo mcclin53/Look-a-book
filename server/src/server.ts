aefce91c0c55682206532519592174ebc856196f
mcclin53 <dan.mcclintic@gmail.com>
2025-05-09 22:13:20 -0400

    updated auth and server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,31 @@ import express from 'express';
 import path from 'node:path';
 import db from './config/connection.js';
 import routes from './routes/index.js';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { typeDefs, resolvers } from './schemas/index.js';
+import { authenticateToken } from './utils/auth.js';
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+const startApolloServer = async () => {
+  await server.start();
+  await db();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use('/graphql', expressMiddleware(server as any,
+    {
+      context: authenticateToken as any
+    }
+  ));
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -18,4 +37,7 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
 });
+
+startApolloServer();
